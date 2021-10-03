362954c3ff9e40e920f0d57fa88226cc6764b8a6
root <root@localhost>
2021-10-03 07:20:33 +0000

    Added member table and base leaderboard template

---
+++ db/migrate/20211002230705_create_members.rb
@@ -0,0 +1,11 @@
+class CreateMembers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :members do |t|
+      t.string :email
+      t.string :name
+      t.integer :points
+
+      t.timestamps
+    end
+  end
+end
