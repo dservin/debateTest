045c7ee724b65eed68a1a5e1cb9dac500a11fb05
root <root@localhost>
2021-10-22 20:11:06 +0000

    host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000' #replace with your own url
+  host = 'https://debate-team-stage-app.herokuapp.com/' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
   
   # SMTP settings for gmail
